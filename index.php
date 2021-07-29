a20706a176d27772c113cc5b0af57e629483636d
ascerra <ascerra@redhat.com>
2021-07-29 15:00:10 -0400

    added new text

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body>
 
-<?php echo "Hello World!"; ?>
+<?php echo "Hello Adam Lets rock this thing!"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 
 <?php $links = [];
